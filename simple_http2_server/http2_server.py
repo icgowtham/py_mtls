f11e3175d072ae4c676e2c63a5ac52c85d333435
Ishwarachandra Gowtham <ic.gowtham@gmail.com>
2020-07-10 23:43:43 +0530

    Cleanup.

--- simple_http2_server/http2_server.py
+++ simple_http2_server/http2_server.py
@@ -2,13 +2,11 @@
 """Entry point."""
 
 from yaml import safe_load
-from web_app import PeerCertWSGIRequestHandler, ssl_context, app as web_app
+from web_app import app as web_app
 
 if __name__ == '__main__':
     with open('config.yaml') as stream:
         config = safe_load(stream)
     web_app.run(host=config.get('server_host', '0.0.0.0'),
                 port=int(config.get('server_port', 9009)),
-                ssl_context=ssl_context,
-                request_handler=PeerCertWSGIRequestHandler,
                 debug=True)
